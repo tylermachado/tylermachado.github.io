4e4ccb3a31f3703b752898b47395dff4f9ab0bfd
tylermachado <tyler.machado@gmail.com>
2015-04-03 11:23:15 -0400

    tablet version

--- sandbox/resources/js/chart.js
+++ sandbox/resources/js/chart.js
@@ -1,15 +1,17 @@
-var spacingBetweenBars = 5;
-var leftoffset = 90;
+var data = [{"aspect":"Action-oriented","United States":40,"Middle East":35,"India":31,"Latin America":27,"Europe":24,"China":22,"Sub-Saharan":26},{"aspect":"Analytical","United States":27,"Middle East":32,"India":30,"Latin America":21,"Europe":38,"China":47,"Sub-Saharan":25.6},{"aspect":"Commercial","United States":45,"Middle East":50,"India":34,"Latin America":27,"Europe":41,"China":34,"Sub-Saharan":12.8},{"aspect":"Creative","United States":16,"Middle East":21,"India":33,"Latin America":33,"Europe":27,"China":21,"Sub-Saharan":41.6},{"aspect":"Strategic","United States":24,"Middle East":9,"India":24,"Latin America":3,"Europe":29,"China":15,"Sub-Saharan":16.8},{"aspect":"Ambitious","United States":6,"Middle East":6,"India":12,"Latin America":6,"Europe":10,"China":6,"Sub-Saharan":9.6},{"aspect":"Authentic","United States":25,"Middle East":28,"India":29,"Latin America":26,"Europe":24,"China":31,"Sub-Saharan":40.8},{"aspect":"Emotionally resilient","United States":14,"Middle East":39,"India":24,"Latin America":20,"Europe":21,"China":21,"Sub-Saharan":7.2},{"aspect":"Engaging","United States":20,"Middle East":12,"India":21,"Latin America":15,"Europe":30,"China":15,"Sub-Saharan":10.4},{"aspect":"Forms bonds","United States":13,"Middle East":10,"India":16,"Latin America":11,"Europe":32,"China":11,"Sub-Saharan":6.4},{"aspect":"Collaborative","United States":3,"Middle East":7,"India":10,"Latin America":3,"Europe":10,"China":7,"Sub-Saharan":4.8},{"aspect":"Self-aware","United States":11,"Middle East":32,"India":14,"Latin America":15,"Europe":20,"China":28,"Sub-Saharan":7.2},{"aspect":"Growth-oriented","United States":35,"Middle East":43,"India":46,"Latin America":67,"Europe":20,"China":40,"Sub-Saharan":43.2},{"aspect":"Authoritative","United States":11,"Middle East":17,"India":17,"Latin America":11,"Europe":22,"China":19,"Sub-Saharan":8.8},{"aspect":"Team-developing","United States":35,"Middle East":31,"India":19,"Latin America":40,"Europe":30,"China":19,"Sub-Saharan":28},{"aspect":"Organized","United States":25,"Middle East":20,"India":18,"Latin America":67,"Europe":24,"China":34,"Sub-Saharan":17.6},{"aspect":"Inclusive","United States":3,"Middle East":6,"India":9,"Latin America":1,"Europe":3,"China":5,"Sub-Saharan":0.8},{"aspect":"Visionary","United States":28,"Middle East":16,"India":17,"Latin America":44,"Europe":19,"China":30,"Sub-Saharan":12},{"aspect":"Experienced","United States":14,"Middle East":15,"India":19,"Latin America":5,"Europe":20,"China":20,"Sub-Saharan":31.2},{"aspect":"Knowledgeable","United States":11,"Middle East":17,"India":21,"Latin America":4,"Europe":12,"China":11,"Sub-Saharan":9.6}];
 
-var margin = {left: 30, bottom: 0, right: 30, top: 0},
+var spacingBetweenBars = 5;
+var leftoffset = 120;
+
+var margin = {left: 0, bottom: 0, right: 0, top: 0},
     height = 3400 - margin.top - margin.bottom,
-    width = window.innerWidth - margin.left - margin.right - leftoffset - 20;
+    width = window.innerWidth - margin.left - margin.right - leftoffset;
 
 var color = d3.scale.ordinal()
-    .range(["#f00404", "#2dbafc", "#2ea51a", "#fc6103", "#01594c", "#012497", "#a815f3"]);
+    .range(["#f00404", "#2dbafc", "#2ea51a", "#fc6103", "#01594c", "#0031D2", "#a815f3"]);
 
 var x = d3.scale.linear()
-    .range([width, 0]);
+    .range([width*.95, 0]);
 
 var y0 = d3.scale.ordinal()
     .rangeRoundBands([height, 0], .4);
@@ -27,14 +29,14 @@ var tickFormatter = function(d) {
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top")
-    .tickValues([0, 10, 20, 30, 40, 50, 60, 67])
+    .tickValues([0, 10, 20, 30, 40, 50, 60, 70])
     .tickSize(-height)
     .tickFormat(tickFormatter);
 
  var xAxisRepeat = d3.svg.axis()
     .scale(x)
     .orient("top")
-    .tickValues([0, 10, 20, 30, 40, 50, 60, 67])
+    .tickValues([0, 10, 20, 30, 40, 50, 60, 70])
     .tickSize(0)
     .tickFormat(tickFormatter);
 
@@ -55,7 +57,7 @@ var svg = d3.select("#d3-container").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
+
   var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "aspect"; });
 
   data.forEach(function(d) {
@@ -69,15 +71,13 @@ d3.tsv("data.tsv", function(error, data) {
         // establish x axis
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(" + leftoffset + ",22)")
+      .attr("transform", "translate(" + leftoffset + ",37)")
       .call(xAxis);
-
-      svg.select(".x.axis .tick text").style("text-anchor","start");
 
         // establish y axis
   svg.append("g")
       .attr("class", "y axis")
-      .attr("transform", "translate(" + leftoffset + ",22)")
+      .attr("transform", "translate(" + leftoffset + ",37)")
       .call(yAxis);
 
         // append a group for each aspect of the data
@@ -97,7 +97,7 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("x", function(d) { return 1; })
       .attr("width", function(d) { return x(d.value); })
       .style("fill", function(d) { return color(d.name); })
-      .attr("transform", "translate(0,0)");
+      .attr("transform", "translate(-1,0)");
 
         // add a rectangle of white space between each aspect group
   var whiteSpace = svg.selectAll('.aspectgroup').append("g")
@@ -105,8 +105,11 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("transform", "translate(-10,-50)")
       .append("rect")
       .style("fill", "white")
-      .attr("width", width + margin.left + margin.right)
+      .attr("width", width+10)
       .attr("height", 45);
+
+      // adjust first bit of white space
+  svg.select(".aspectgroup g rect").attr("height","30").attr("transform", "translate(0,15)");
 
         // add the subcategory text between aspect groups, on top of the white space
   svg.append("g")
@@ -114,7 +117,7 @@ d3.tsv("data.tsv", function(error, data) {
       .call(yAxisText)
     .selectAll("text")
       .attr("y", "-68")
-      .attr("x", "1")
+      .attr("x", "0")
       .attr("class","subcategory");
 
         // add text labels for each country along Y axis
@@ -135,22 +138,22 @@ d3.tsv("data.tsv", function(error, data) {
           .append("text")
           .attr("class", "category")
           .text("Thinking");
-  var categoryMotivation = svg.select('.aspectgroup:nth-child(8)')
+  var categoryMotivation = svg.select('.aspectgroup:nth-child(15)')
           .attr("class", "aspectgroup categorygroup")
           .append("text")
           .attr("class", "category")
           .text("Motivation");
-  var categorInterpersonal = svg.select('.aspectgroup:nth-child(11)')
+  var categorInterpersonal = svg.select('.aspectgroup:nth-child(18)')
           .attr("class", "aspectgroup categorygroup")
           .append("text")
           .attr("class", "category")
           .text("Interpersonal");
-  var categorySelfawareness = svg.select('.aspectgroup:nth-child(14)')
+  var categorySelfawareness = svg.select('.aspectgroup:nth-child(13)')
           .attr("class", "aspectgroup categorygroup")
           .append("text")
           .attr("class", "category")
           .text("Self-Awareness");
-  var categoryLeadership = svg.select('.aspectgroup:nth-child(16)')
+  var categoryLeadership = svg.select('.aspectgroup:nth-child(8)')
           .attr("class", "aspectgroup categorygroup")
           .append("text")
           .attr("class", "category")
@@ -170,39 +173,43 @@ d3.tsv("data.tsv", function(error, data) {
           // add the extra X axes with big categories
   var extraAxisText = svg.selectAll('.categorygroup').append("g")
       .attr("transform", "translate(0,-30)")
+      .attr("class","x axis")
       .call(xAxisRepeat)
       .selectAll("text")
       .attr("class","xAxisRepeat");
 
-  svg.append("text")      // text label for the x axis
+        // adjust text anchor for first and last labels
+      svg.selectAll(".x.axis .tick:nth-child(1) text").style("text-anchor","start");
+      svg.selectAll('.x.axis .tick:nth-child(8) text').style("text-anchor","end");
+
+        // create space for x axis label
+  svg.append("text")  
         .attr("x", 0 )
         .attr("y",  10 )
-        .attr("transform", "translate(90,0)")
+        .attr("transform", "translate(" + (leftoffset-10) + ",0)")
         .attr("class", "axislabel")
         .style("text-anchor", "end");
 
+        // add text to x axis label
   svg.select(".axislabel")
         .append("tspan")
         .attr("x", "0")
         .attr("dy", "0")
         .text("Percentage of");
-
   svg.select(".axislabel")
         .append("tspan")
         .attr("x", "0")
         .attr("dy", "1em")
         .text("Executives With");
-
-  svg.select(".axislabel")
+ svg.select(".axislabel")
         .append("tspan")
         .attr("x", "0")
         .attr("dy", "1em")
         .text("Strength in Area");
 
+        // force line break for Sub-Saharan Africa bar
   svg.selectAll(".legend").filter(":nth-child(15)").select("text").append("tspan")
         .attr("x", "-10")
         .attr("dy", "1em")
         .text("Africa");
 
-
-});
