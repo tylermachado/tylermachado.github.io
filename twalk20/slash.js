6bb2c2b64986503b649e2843b03b906aa427a8cf
tyler-machado <tyler.machado@hbr.org>
2016-02-24 14:19:19 -0500

    adding q string

--- twalk20/slash.js
+++ twalk20/slash.js
@@ -1,5 +1,8 @@
-var myCar = new Object();
-myCar.year = 1969;
-myCar.text = "Everything works, cool";
+function(name, url) {
+	if (!url) url = window.location.href;
+	var myCar = new Object();
+	myCar.year = 1969;
+	myCar.text = "Everything works, cool";
 
-return JSON.stringify(myCar);
+	return JSON.stringify(myCar);
+}
