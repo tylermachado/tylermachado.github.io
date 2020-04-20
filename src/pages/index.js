497327545ac5dff38906add6d588198a197d9f42
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-04-20 17:42:58 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header.jsx"
+
+export default () => (
+    <div>
+        <Header />
+        <img src="https://source.unsplash.com/random/600x500" alt="" />
+    </div>
+)
