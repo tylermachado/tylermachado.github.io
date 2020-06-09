6dd54e4eeb56113ecb6c20d61f13879f83850215
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-06-09 19:32:00 -0400

    font size edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ export default () => (
             <div className="herotext">
                 <h1>Hello, I'm Tyler.</h1>
                 <p>I'm a design-minded, data-literate web developer with a decade of work in digital news media. I like to build creative, illuminating experiences for users, backed by robust processes for producers.</p>
+                <p>Feel free to get in touch! My email address is tyler.machado [at] gmail.com</p>
             </div>
         </section>
 
