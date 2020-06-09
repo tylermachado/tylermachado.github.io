8770abd318710d082f7cba38c338c09445421497
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-06-09 18:38:51 -0400

    Updates

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default (props) => (
-    <footer>
-        <div>&copy; 2020 Tyler Machado.</div>
-        <div>This site was built with: HTML, CSS, <a href="https://gatsbyjs.org/">Gatsby</a>, and <a href="//fonts.google.com/">Google Fonts</a>.</div>
-    </footer>
-)
