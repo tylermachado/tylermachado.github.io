9556251fdad7777ec642781ae8dd7ca8d36bbf7b
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-06-16 17:52:04 -0400

    svisual updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 
+let date = new Date();
+
 export default (props) => (
     <footer>
-        <div>&copy; 2020 Tyler Machado.</div>
-        <div>This site was built with: HTML, CSS, <a href="https://gatsbyjs.org/">Gatsby</a>, and <a href="//fonts.google.com/">Google Fonts</a>.</div>
+        <div>&copy; {date.getFullYear()} Tyler Machado.</div>
+        <div>This site was built with: HTML, CSS, <a href="https://gatsbyjs.org/">Gatsby</a>, <a href="//sheets.google.com/">Google Sheets</a>, and <a href="//fonts.google.com/">Google Fonts</a>.</div>
     </footer>
 )
