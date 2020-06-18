5884bcd43970fdbd11df9c8eea8445c957129ce5
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-06-18 18:49:01 -0400

    pause on markdown pages plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,12 @@ module.exports = {
   plugins: [
       `gatsby-plugin-react-helmet`,
       `gatsby-transformer-remark`,
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `markdown-pages`,
-          path: `${__dirname}/src/markdown-pages`,
-        },
-      }
+      // {
+      //   resolve: `gatsby-source-filesystem`,
+      //   options: {
+      //     name: `markdown-pages`,
+      //     path: `${__dirname}/src/markdown-pages`,
+      //   },
+      // }
   ]
 }
