f9e9ee67e118b3c0ab39285e92fe8bf6faa4750e
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-07-01 21:58:28 -0400

    fix nav background colors

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,12 +3,14 @@ import "../styles/nav.scss"
 
 export default (props) => (
     <header id="nav">
-        <div className="container">
-            <ul>
-                <li><a href="/#work">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#talks">Talks</a></li>
-            </ul>
+        <div className="backdrop">
+            <div className="container">
+                <ul>
+                    <li><a href="/#work">Projects</a></li>
+                    <li><a href="/#resume">Resume</a></li>
+                    <li><a href="/#talks">Talks</a></li>
+                </ul>
+            </div>
         </div>
         <div class="triangle-bottom-nav"></div>
     </header>
