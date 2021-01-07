156a64758ddb1cf6a98d772e732cc04faea67d1d
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-01-07 14:49:40 -0500

    hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default (props) => (
             </div>
             <div className="herotext">
                 <h1>Hi, I'm Tyler Machado.</h1>
-                <p>I'm a front-end web developer, designer and editor with a decade of experience in digital news. I use technology and design to build creative, illuminating experiences and tools. I'm data-literate, process-oriented, and highly organized (in that spreadsheet-for-<em>everything</em> type of way).</p>
+                <p>I'm a front-end web developer designer, and data visualization specialist with a decade of experience in digital news. I use technology and design to build engaging, illuminating tools and creative new ways to tell stories. I'm statistically literate, process-oriented, and highly organized (in that spreadsheet-for-<em>everything</em> type of way).</p>
             </div>
         </div>
         <div class="triangle-bottom">
