e70c516e5a0c0366796fe24ea803ee4f51cb2c5f
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-01-19 19:46:45 -0500

    implement new version of masonry with expanding cards

--- src/components/PortfolioCards.jsx
+++ src/components/PortfolioCards.jsx
@@ -1,38 +1,66 @@
 import React from "react"
+import Masonry from 'react-masonry-component';
 import "../styles/masonry.scss"
 import portfolio from "../../content/portfolio.json"
 import SectionHeader from "../components/SectionHeader.jsx"
 
-const PortfolioCards = () => (
-    <div class="container">
-        <div id="work">
-          <SectionHeader words="Selected Work" />
-          <div id="colsContainer" className="cols">
-              {portfolio
-                  .filter((project) => {return project.show === 1})
-                  .map((project, index) =>
-                  {
-                    return (
+class PortfolioCards extends React.Component {
+    constructor(props) {
+        super(props);
+        this.clickActive = this.clickActive.bind(this);
+    }
 
-                        <div key={`content_item_${index}`} className="portfolio-card">
-                            <a href={project.link}>
-                                <div>
-                                    <img alt={project.title} src={`../../static/screenshots/${project.screenshot}`} />
-                                    <div>{project.title}</div>
-                                    {
-                                        project.publication === 'personal' ?
-                                        '' :
-                                        <div className="subhed">{project.publication}</div>
-                                    }
-                                </div>
-                            </a>
+    state = {
+        clicked: null
+    }
+
+    clickActive(ind) {
+        this.setState({clicked: ind})
+        // this.masonry.layout()
+    }
+
+    render() {
+        const childElements = portfolio
+            .filter((project) => {return project.show === 1})
+            .map((project, index) => {
+                return (
+                    <div
+                    key={`content_item_${index}`}
+                    className={this.state.clicked === index ? "portfolio-card active" : "portfolio-card"}
+                    onClick={() => this.clickActive(index)}>
+                        <img alt={project.title} src={`../../static/screenshots/${project.screenshot}`} />
+                        <div className="title">{project.title}</div>
+                        {
+                            project.publication === 'personal' ?
+                            '' :
+                            <div className="subhed">{project.publication}</div>
+
+                        }
+                        <div className="blurb">
+                            <span dangerouslySetInnerHTML={{__html:project.blurb}}></span>
+                            &nbsp;
+                            <a href={project.link}>check it out</a>
                         </div>
+                    </div>
+                )
+            });
 
-
-                    )
-              })}
-          </div>
-        </div>
-    </div>
-)
+        return (
+            <div class="container">
+                <div id="work">
+                  <SectionHeader words="Selected Work" />
+                      <Masonry
+                            className={'masonry-container'}
+                            elementType={'div'}
+                            disableImagesLoaded={false}
+                            updateOnEachImageLoad={false}
+                            enableResizableChildren={true}
+                        >
+                            {childElements}
+                        </Masonry>
+                </div>
+            </div>
+        )
+    } //render
+} // class PortfolioCards
 export default PortfolioCards
