c37898b76745852f5740359829f7ea18993907f6
tylermachado <3586466+tylermachado@users.noreply.github.com>
2023-08-21 16:04:29 -0400

    portfolio updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default (props) => (
             </div>
             <div className="about__text-container container fadein">
                 <div className="about__text">
-                    <h1>Hi, I'm Tyler Machado.</h1>
+                    <h1>Tyler Machado</h1>
                     <p>I'm a front-end web developer and digital designer from Boston. I do my best work in hybrid roles where data, design and development come together — building digital experiences that are both visually engaging and delightfully usable.</p>
                     <p>I've got more than a decade of experience in digital media, working across agency, in-house and freelance positions, with a focus on collaborating with nonprofit and independent media organizations. I've co-led conference sessions at SRCCON events and Boston University’s Storytelling With Data workshops.</p>
                 </div>
