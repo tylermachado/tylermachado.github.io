3e6feffcc1a564bf05c16e48ff9c3b369c34c900
tylermachado <3586466+tylermachado@users.noreply.github.com>
2023-08-22 18:57:59 -0400

    new hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/hero.scss"
+
+export default (props) => (
+    <section id="hero">
+        <div className="hero__container">
+            <div className="hero__image-container">
+            </div>
+            <div className="hero__text fadein">
+                <h1>Tyler Machado</h1>
+            </div>
+        </div>
+    </section>
+)
