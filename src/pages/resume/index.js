8611f4a193eaccceb4a83fd88ffeb6e519a525f4
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-02-10 15:01:42 -0500

    get resume working

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Accordion from '../../components/accordion'
 import Navbar from '../../components/navbar'
 import './index.scss';
 
-const Resume = () => (
-  <>
-    <Navbar />
-    <main>
-      <Accordion
-        title="Alley"
-        subtitle="UX Developer II, March 2021-present"
-        content="llorem ipsum"
-      />
-    </main>
-  </>
-);
+const Resume = () => {
+
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {
+    let url = "/content/resume.json";
+    fetch(url)
+        .then(res => res.json().then(content => setContent(content)));
+    }, []);
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        {content.map((item, index) => (
+          <Accordion
+            key={index}
+            title={item.workplace}
+            subtitle={item.titles[0]}
+            content={item.duties}
+          />
+        ))}
+      </main>
+    </>
+  );
+};
 
 export default Resume;
