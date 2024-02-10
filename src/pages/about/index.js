e13d62011ce7ade7564d66552492c9da42eeefb2
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-02-10 15:31:52 -0500

    About page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,8 +9,15 @@ const About = () => (
     <main>
       <div className="About">
         <p>
-          This is the about page.
+          Hello, I'm Tyler. I design and build things on the web.
         </p>
+        <p>
+          I like working where data, design and engineering come together, building intuitive, accessible, and engaging experiences. I’ve worked in various roles in digital media for more than a decade, and I collaborate best with small, interdisciplinary teams that punch above their weight. 
+        </p>
+        <p>
+          I live in Boston, with my partner and my cat.
+        </p>
+        <p>You can reach me at tyler.machado@gmail.com.</p>
       </div>
     </main>
   </>
