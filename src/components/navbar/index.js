2b5dc3a3f8f07ded92bc959c0dda4df232542e58
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-02-12 15:28:26 -0500

    accordion work

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -42,8 +42,8 @@ const Navbar = () => {
             <li><a href="/#/portfolio">Portfolio</a></li>
             <li><a href="/#/resume">Resume</a></li>
             <li className="socials">
-              <a href="//github.com/tylermachado"><FaSquareGithub /></a>
-              <a href="//linkedin.com/in/tylermachado"><FaLinkedin /></a>
+              <a href="//github.com/tylermachado" name="Tyler Machado on GitHub"><FaSquareGithub /></a>
+              <a href="//linkedin.com/in/tylermachado" name="Tyler Machado on LinkedIn"><FaLinkedin /></a>
             </li>
           </ul>
         </nav>
