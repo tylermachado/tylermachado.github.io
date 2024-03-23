670d373a12cbadb8fe5bca9346daedb9c15ec259
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-03-23 13:31:09 -0400

    add new title sldie

---
+++ src/sections/title/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.scss';
+
+const Title = () => (
+  <>
+    <section id="Title">
+      <h1>Tyler Machado</h1>
+      <ul>
+        <li>Data Visualization</li>
+        <li>UI/UX Design</li>
+        <li>Frontend Development</li>
+      </ul>
+    </section>
+  </>
+);
+
+export default Title;
