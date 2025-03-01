b9e74f105da6f124deb04a6f54b312fd05907eab
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-01 15:19:11 -0500

    install Flowbite and establish nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+
 	import '../app.css';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	
 	let { children } = $props();
 </script>
 
@@ -9,7 +11,7 @@
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tyler Machado</span>
   </NavBrand>
   <NavHamburger  />
-  <NavUl >
+  <NavUl>
     <NavLi href="/">Home</NavLi>
     <NavLi href="/portfolio">Portfolio</NavLi>
     <NavLi href="/resume">Resume/CV</NavLi>
