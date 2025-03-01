9d7c831e8c1dba4d6aabae68df757f4a536a1908
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-01 15:32:46 -0500

    work with sidebar

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,3 +1,51 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+// @ts-nocheck
+
+  import { AccordionItem, Accordion } from 'flowbite-svelte';
+  import { Sidebar, SidebarBrand, SidebarCta, SidebarDropdownItem, SidebarDropdownWrapper, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+</script>
+
+<Sidebar>
+  <SidebarWrapper>
+    <SidebarGroup>
+      <SidebarItem label="Dashboard" href="/">
+        <svelte:fragment slot="icon">
+        </svelte:fragment>
+      </SidebarItem>
+      <SidebarItem label="Accordion" href="/docs/components/accordion">
+        <svelte:fragment slot="icon">
+        </svelte:fragment>
+      </SidebarItem>
+    </SidebarGroup>
+  </SidebarWrapper>
+</Sidebar>
+
+<h2>Work Experience</h2>
+
+<Accordion>
+  <AccordionItem flush>
+    <span slot="header">
+      <h3>self-employed</h3>
+      <span>Freelance Data Visualization Developer, April 2021-present</span>
+    </span>
+    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+    <p class="text-gray-500 dark:text-gray-400">
+      Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
+      and start developing websites even faster with components on top of Tailwind CSS.
+    </p>
+  </AccordionItem>
+  <AccordionItem>
+    <span slot="header">My Header 2</span>
+    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+    <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+    <ul class="list-disc ps-5 dark:text-gray-400 text-gray-500">
+      <li>
+        <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Lorem ipsum </a>
+      </li>
+      <li>
+        <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Tailwind UI </a>
+      </li>
+    </ul>
+  </AccordionItem>
+</Accordion>
