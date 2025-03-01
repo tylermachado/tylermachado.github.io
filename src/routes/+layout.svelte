f0cf2c30e80224715b22771ed4079d148f9138cd
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-01 15:13:43 -0500

    install Flowbite and establish nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 	let { children } = $props();
 </script>
+
+<Navbar>
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tyler Machado</span>
+  </NavBrand>
+  <NavHamburger  />
+  <NavUl >
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/portfolio">Portfolio</NavLi>
+    <NavLi href="/resume">Resume/CV</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
