3392d60cc6b464386bd7a4fac212d8a4b84d0273
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-02 09:52:23 -0500

    new homepage

--- google.config.js
+++ google.config.js
@@ -3,4 +3,8 @@ export default [
 		"id": "1Bj9_mUmlIX49KpCaTVpwcPdVY4B7a6XdiEt2sdl2Fdg",
 		"filepath": "src/data/resume.json"
 	},
+	{
+		"id": "1huLL8SS0k7EB7LanizpBKcmD5MwtFGq8wv0tJOFk_3c",
+		"filepath": "src/data/portfolio.json"
+	}
 ]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,19 @@
   import { Alert } from 'flowbite-svelte';
 </script>
 
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
-  </Alert>
+<div class="col-span-1">
+
+</div>
+
+<div class="p-8 col-span-5">
+  <p>I make websites, dashboards, data visualizations, and design systems.</p>
+
+  <p>I approach my work with a mix of technical experience, design mindset, and journalistic training. I’ve been building creative, engaging, accessible digital experiences for more than a decade, having collaborated with both enterprise-scale international media websites and tiny startup nonprofits. These days I'm particularly interested in working with ambitious small teams and mission-driven organizations.</p>
+
+  <p>My toolkit these days includes (but is not limited to): Svelte, React, D3.js, Python, WordPress block themes.</p>
+
+  <p>When I’m not at my desk, you’ll most likely find me at the restaurants, rock clubs, and soccer fields of New England. I live in Boston with my partner, my cat, and entirely too many records.</p>
+
+  <p>I’m currently working on freelance projects, and I'm looking to book more freelance work or fractional roles through spring and summer of 2025. </p>
+
 </div>
