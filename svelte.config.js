17a5b6bb24bfe499280195e628b263afb2ba5083
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-05 20:56:09 -0600

    trying things

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
@@ -8,9 +10,9 @@ const config = {
 	kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.BASE_PATH || '', // Set in GitHub Actions
+      base: dev ? '' : '/tylermachado.github.io', // Use the repo name as the base in production
     },
-    appDir: 'app', // Avoids conflicts with GitHub Pages
+    appDir: 'app',
   }
 };
 
