7db85c32da4698c5bf94e19dc7416cf2d94b71ac
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-05 20:58:19 -0600

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : '/tylermachado.github.io', // Use the repo name as the base in production
+      base: '',
     },
     appDir: 'app',
   }
