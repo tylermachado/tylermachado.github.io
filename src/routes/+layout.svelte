62037e928284186b1c939e938942195cc0a51649
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-08 16:11:00 -0600

    revamp header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,42 @@
 <script lang="ts">
-
-    import { base } from '$app/paths';
+	import { base } from '$app/paths';
 
 	import '../app.css';
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-  import SocialIcons from '../components/SocialIcons.svelte';
-	
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import SocialIcons from '../components/SocialIcons.svelte';
+
 	let { children } = $props();
 </script>
 
-<main class="site-container">
-<div class="nav-container">
-	<Navbar class="max-w-5xl mx-auto bg-transparent">
-		<NavBrand href="/">
-			<img src="/images/icon.png" alt="Tyler Machado" class="h-10 w-10 rounded-full mr-4" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tyler Machado</span>
-		</NavBrand>
-		<NavHamburger  />
-		<NavUl>
-			<NavLi href="{base}/">Home</NavLi>
-			<NavLi href="{base}/portfolio">Portfolio</NavLi>
-			<NavLi href="{base}/resume">Resume/CV</NavLi>
-			<NavLi href="{base}/contact">Contact</NavLi>
-		</NavUl>
-	</Navbar>
-</div>
+<header>
+		<Navbar class="mx-auto max-w-5xl bg-transparent">
+			<NavBrand href="/">
+				<img src="/images/icon.png" alt="Tyler Machado" class="mr-4 h-10 w-10 rounded-full" />
+				<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+					>Tyler Machado</span
+				>
+			</NavBrand>
+			<NavHamburger />
+			<NavUl>
+				<NavLi href="{base}/">Home</NavLi>
+				<NavLi href="{base}/portfolio">Portfolio</NavLi>
+				<NavLi href="{base}/resume">Resume/CV</NavLi>
+				<NavLi href="{base}/contact">Contact</NavLi>
+			</NavUl>
+		</Navbar>
+</header>
 
-<div class="grid md:grid-cols-6 gap-4 max-w-5xl mx-auto content">
-{@render children()}
-</div>
+<main class="site-container">
+	
+
+	<div class="content mx-auto grid max-w-5xl gap-4 md:grid-cols-6">
+		{@render children()}
+	</div>
 </main>
 
-<footer class="h-[100px] w-full px-10 py-5 bg-green-950 text-white align-right">
-	<div class="max-w-5xl mx-auto">
-    <span class="text-sm">© 2025 Tyler Machado</span>
+<footer class="align-right h-[100px] w-full bg-green-950 px-10 py-5 text-white">
+	<div class="mx-auto max-w-5xl">
+		<span class="text-sm">© 2025 Tyler Machado</span>
 		<SocialIcons lightIcons />
 	</div>
-	</footer>
+</footer>
