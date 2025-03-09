a5fa2c760368f7a0e5153afb63c2cc4fe1e4c857
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-03-09 14:33:00 -0500

    formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,21 +9,21 @@
 </script>
 
 <header>
-		<Navbar class="mx-auto max-w-5xl bg-transparent">
-			<NavBrand href="/">
-				<img src="/images/icon.png" alt="Tyler Machado" class="mr-4 h-10 w-10 rounded-full" />
-				<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-					>Tyler Machado</span
-				>
-			</NavBrand>
-			<NavHamburger />
-			<NavUl>
-				<NavLi href="{base}/">Home</NavLi>
-				<NavLi href="{base}/portfolio">Portfolio</NavLi>
-				<NavLi href="{base}/resume">Resume/CV</NavLi>
-				<NavLi href="{base}/contact">Contact</NavLi>
-			</NavUl>
-		</Navbar>
+	<Navbar class="mx-auto max-w-5xl bg-transparent">
+		<NavBrand href="/">
+			<img src="/images/icon.png" alt="Tyler Machado" class="mr-4 h-10 w-10 rounded-full" />
+			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+				>Tyler Machado</span
+			>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl>
+			<NavLi href="{base}/">Home</NavLi>
+			<NavLi href="{base}/portfolio">Portfolio</NavLi>
+			<NavLi href="{base}/resume">Resume/CV</NavLi>
+			<NavLi href="{base}/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
 
 <main class="site-container">
