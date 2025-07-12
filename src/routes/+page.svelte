0f7d43bc4ede3c8766d96ddb83994672204aeca6
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-07-12 11:21:41 -0400

    Fix extra text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,7 @@
   <img src="/images/headshot.jpg" alt="Tyler Machado" class="rounded-2xl md:w-48 md:h-48 mb-8 hidden md:block mx-auto md:float-right md:mx-8" />
 
   <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a creative technologist</span> from Boston.</p>
-
-  <p>I live in Boston, MA and I make websites, dashboards, data visualizations, and design systems.</p>
-
-  <p>I’ve been a web developer and design technologist for more than a decade, helping mission-driven organizations tell stories and inform people through digital, visual, data-driven experiences. For me that’s meant creating data-driven interactive essays, fleshing out design systems for modern rebrands, auditing and implementing web accessibility improvements, and hacking on WordPress to help customize and improve the CMS experience — to name just some of the work that I like to do.</p>
-
-  <p>I’m currently working on freelance projects, and I have capacity for more contract work or fractional roles for spring/summer 2025. </p>
-
+  
   <p>I make websites, dashboards, data visualizations, and design systems that help mission-driven organizations and publishers make the most of visual, digital storytelling. Across 10+ years of working on the web, my career has included:</p>
 
   <ul>
