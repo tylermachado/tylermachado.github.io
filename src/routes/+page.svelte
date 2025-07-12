87f7ad51faaecfdbf14d124412481504c0f05546
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-07-12 11:18:50 -0400

    new intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,25 @@
 <div class="col-span-4">
   <img src="/images/headshot.jpg" alt="Tyler Machado" class="rounded-2xl md:w-48 md:h-48 mb-8 hidden md:block mx-auto md:float-right md:mx-8" />
 
-  <p class="text-2xl font-bold">Hi, I'm Tyler.</p>
+  <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a creative technologist</span> from Boston.</p>
 
   <p>I live in Boston, MA and I make websites, dashboards, data visualizations, and design systems.</p>
 
   <p>I’ve been a web developer and design technologist for more than a decade, helping mission-driven organizations tell stories and inform people through digital, visual, data-driven experiences. For me that’s meant creating data-driven interactive essays, fleshing out design systems for modern rebrands, auditing and implementing web accessibility improvements, and hacking on WordPress to help customize and improve the CMS experience — to name just some of the work that I like to do.</p>
 
-  <p>I’m currently working on freelance projects, and I have capacity for more contract work or fractional roles for spring/summer 2025. <a href="{base}/contact">Want to work together?</a></p>
+  <p>I’m currently working on freelance projects, and I have capacity for more contract work or fractional roles for spring/summer 2025. </p>
+
+  <p>I make websites, dashboards, data visualizations, and design systems that help mission-driven organizations and publishers make the most of visual, digital storytelling. Across 10+ years of working on the web, my career has included:</p>
+
+  <ul>
+    <li>📊 Designing, building and writing data stories and interactives that turn numbers into engaging visual narratives</li>
+    <li>🎨 Working on UX, frontend, and branding to help organizations connect with their communities</li>
+    <li>📝 Customizing content management systems (especially WordPress) to help journalists do their jobs in the most web-savvy, pain-free ways possible</li>
+  </ul> 
+
+<p>When I’m not at my desk, you’ll most likely find me at the restaurants, rock clubs, and soccer fields of New England.</p>
+
+<p>I'm currently splitting my time between building WordPress sites with my friends at Modern Tribe and taking on freelance dataviz and frontend projects. <a href="{base}/contact">Want to work together?</a></p>
 
   <SocialIcons lightIcons={false} className={"mt-5"} size={8} />
 
