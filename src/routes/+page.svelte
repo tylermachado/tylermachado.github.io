7f3dc2a729d8b294292141e7798a541384f721f3
tylermachado <3586466+tylermachado@users.noreply.github.com>
2025-08-18 19:49:44 -0400

    intro copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,12 @@
 
   <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a creative technologist</span> from Boston.</p>
 
-  <p>I work on digital tools — like data visualizations, dashboards, and websites — that help mission-driven organizations tell compelling stories and connect with their communities. My experience includes:</p>
+  <p>I design and build digital tools — like data visualizations, dashboards, and websites — that support and elevate digital storytelling. Whether I'm visualizing complex data, developing WordPress platforms, or translating editorial needs into thoughtful UX, I'm here to help mission-driven teams communicate clearly and connect with their audiences on the modern web. My experience includes:</p>
 
   <ul>
-    <li>📊 Creating <span class="font-bold">data-driven stories and interactive visualizations</span> to make narratives from numbers</li>
-    <li>🎨 Crafting digital identity through <span class="font-bold">brand graphics, design systems and frontend implementation</span></li>
-    <li>📝 Customizing <span class="font-bold">WordPress</span> platforms to empower journalists, editors and storytellers</li>
+    <li>📊 Shaping data into <span class="font-bold">interactive stories and dashboards</span> that make narratives from numbers</li>
+    <li>🎨 Crafting <span class="font-bold">branding kits, design systems and UX/UI components</span> that align with brand identity</li>
+    <li>📝 Developing flexible <span class="font-bold">CMS platforms</span> for journalists, storytellers, and nonprofits</li>
   </ul>
 
 <p>When I’m not at my desk, you’ll most likely find me at the restaurants, rock clubs, and soccer fields of New England.</p>
