2c6aa4a8cd7b033e6a949c1ebd0a406142db316a
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-03-07 08:37:33 -0500

    copy update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,17 +11,19 @@
 
   <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a freelance design technologist and UX/UI engineer</span> from Boston.</p>
 
-  <p>I design and build data-driven web apps, dashboards and systems for nonprofit and editorial teams, bridging product, design, data, and code. What I can help with:</p>
+  <p>I design and build data-driven web apps, dashboards and systems for nonprofit and editorial teams, bridging product, design, data, and code. You may know me from such hits as <a href="https://pudding.cool/2024/10/nba-uniforms/">the Pudding's NBA uniforms deep dive</a>, the <a href="https://www.tylermachado.com/nicar-schedule/">alternative conference schedule grid for NICAR 2026</a>, and <a href="https://www.tylermachado.com/archive/dash-brief.pdf">Northeastern's campus COVID testing dashboard</a>.</p>
+    
+  <p>What I can help with:</p>
 
   <ul>
-    <li>📊 Turning complex data into <span class="font-bold">engaging, interactive narratives and decision-ready dashboards</span></li>
+    <li>📊 Turning data into <span class="font-bold">engaging, interactive narratives and decision-ready dashboards</span></li>
     <li>🎨 Crafting <span class="font-bold">branding kits, design systems and component-driven frontends</span> that align with editorial voice and brand identity</li>
     <li>📝 Developing flexible, content-first <span class="font-bold">WordPress and Next.js sites</span> for journalists, storytellers, and nonprofits</li>
   </ul>
 
-<p>When I’m not at my desk, you’ll most likely find me at the restaurants, rock clubs, and soccer fields of New England.</p>
+<p>When I’m not at my desk, you’ll most likely find me at the restaurants and rock clubs of New England, or cheering on our local women's soccer team with the <a href="//bostonisa.com/">Boston ISA</a>.</p>
 
-<p><span class="font-bold">I'm always looking to collaborate with teams that value a healthy blend of design, data, and storytelling to make an impact, and I'm currently available for freelance, contract, or full-time work!</span> <a href="{base}/contact">Want to work together?</a></p>
+<!-- <p><span class="font-bold">I'm always looking to collaborate with teams that value a healthy blend of design, data, and storytelling to make an impact, and I'm currently available for freelance, contract, or full-time work!</span> <a href="{base}/contact">Want to work together?</a></p> -->
 
   <SocialIcons lightIcons={false} className={"mt-5"} size={8} />
 
