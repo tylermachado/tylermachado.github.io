1bc09cef6d74ba6d51e0325c743cf0aaccadbb31
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-07-29 18:18:38 -0400

    copy update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,16 @@
 <div class="col-span-4">
   <img src="/images/headshot.jpg" alt="Tyler Machado" class="rounded-2xl md:w-48 md:h-48 mb-8 hidden md:block mx-auto md:float-right md:mx-8" />
 
-  <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a design technologist and front-end engineer</span> from Boston.</p>
+  <p class="text-2xl">Hi, I'm Tyler and <span class="font-bold">I'm a design technologist and frontend engineer</span> from Boston. I'm interested in digital-visual storytelling, and the systems, platforms, and people that make it happen.</p>
 
-  <p>I'm interested in digital-visual storytelling, and the systems and platforms that power them. I've spent more than a decade designing and building websites, dashboards, and data-driven interactives for mission-driven organizations and editorial teams, bridging product, design, data, and code. You may know me from such hits as <a href="https://pudding.cool/2024/10/nba-uniforms/">the Pudding's NBA uniforms deep dive</a>, the <a href="https://www.tylermachado.com/nicar-schedule/">alternative conference schedule grid for NICAR 2026</a>, and <a href="https://www.tylermachado.com/archive/dash-brief.pdf">Northeastern's campus COVID testing dashboard</a>.</p>
+  <p>I've spent more than a decade designing and building websites, dashboards, and data-driven interactives for mission-driven organizations and editorial teams, bridging product, design, data, and code, and I have a track record of helping small teams punch above their weight on the modern web. You may know me from such hits as <a href="https://pudding.cool/2024/10/nba-uniforms/">the Pudding's NBA uniforms deep dive</a>, the <a href="https://www.tylermachado.com/nicar-schedule/">alternative conference schedule grid for NICAR 2026</a>, and <a href="https://www.tylermachado.com/archive/dash-brief.pdf">Northeastern's campus COVID testing dashboard</a>.</p>
     
   <p>What I can help with:</p>
 
   <ul>
     <li>📊 Transforming complex datasets into <span class="font-bold">engaging, clarifying, interactive narratives and dashboards</span></li>
-    <li>🎨 Crafting <span class="font-bold">branding kits, design systems and component-driven frontends</span> that support standout editorial voice and brand identity</li>
-    <li>📝 Developing <span class="font-bold">flexible, content-first sites</span> for journalists, storytellers, and nonprofits, in WordPress as well as emerging content management systems</li>
+    <li>🎨 Crafting <span class="font-bold">branding kits, design systems and component-driven frontends</span> that establish identity and editorial voice for mission-driven organizations</li>
+    <li>📝 Developing <span class="font-bold">bespoke, content-driven digital platforms</span> that help journalists and nonprofits deliver stories and information to the world</li>
   </ul>
 
 <p>I'm currently the Senior Web Developer/Designer at <a href="https://www.inovalon.com/">Inovalon</a>, and I also design digital and physical graphics for <a href="//bostonisa.com/">Boston ISA</a>, the independent supporters' group for the Boston Legacy FC soccer team. When I’m not at my desk, you’ll most likely find me at the restaurants, rock clubs, and soccer fields of New England. I live in Boston with my partner, my cat, and entirely too many records.</p>
